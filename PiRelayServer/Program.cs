647d85317b462dad1b180acc024959baaf31ffb3
Booquiefious <31057833+Booquiefious@users.noreply.github.com>
2019-01-31 00:45:41 -0600

    Fix
    Fix

--- PiRelayServer/Program.cs
+++ PiRelayServer/Program.cs
@@ -4,18 +4,9 @@ namespace PiRelayServer
 {
     class Program
     {
-        int num = 1;
-
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-        }
-
-        void Count()
-        {
-            Console.WriteLine(num);
-            num++;
-            Count();
         }
     }
 }
