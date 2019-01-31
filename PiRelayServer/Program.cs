191ca783e2278abff5bfbc3117b90d208dfe248c
Booquiefious <31057833+Booquiefious@users.noreply.github.com>
2019-01-31 00:42:40 -0600

    Add project files.

---
+++ PiRelayServer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PiRelayServer
+{
+    class Program
+    {
+        int num = 1;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        void Count()
+        {
+            Console.WriteLine(num);
+            num++;
+            Count();
+        }
+    }
+}
